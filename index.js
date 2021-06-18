f118d97c8ab0b6d8b80f391c1450c282fc787e6e
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-18 09:52:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const { ShardingManager } = require('kurasuta');
+const { Constants, Intents, Util } = require('discord.js');
+const { join } = require('path');
+const { token } = require('./config.json');
+const mongoose = require('mongoose');
+const { mongo } = require('../config.json');
+
+const { GUILDS, GUILD_MEMBERS, GUILD_BANS, GUILD_VOICE_STATES, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS } = Intents.FLAGS;
+
+const SenkuClient = require('./src/Senku.js');
+
+const customClientOptions = {
+    messageCacheMaxSize: 1,
+    messageCacheLifetime: 1800,
+    disableMentions: 'everyone',
+    restRequestTimeout: 30000,
+    intents: [ GUILDS, GUILD_MEMBERS, GUILD_BANS, GUILD_VOICE_STATES, GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS ]
+};
+
+const sharderOptions = {
+    clientOptions: Util.mergeDefault(Constants.DefaultOptions, customClientOptions),
+    client: SenkuClient,
+    timeout: 90000,
+    token
+};
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useUnifiedTopology : true,
+    useNewUrlParser: true,
+}).then(console.log('connected to mongo db'));
+
+const sharder = new ShardingManager(join(__dirname, '/src/SenkuBaseCluster.js .js'), sharderOptions);
+
+sharder.spawn();
+
