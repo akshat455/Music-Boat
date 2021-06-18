9b27a16cc00a26c220a1986ae17338f237262585
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-18 09:54:25 +0530

    Create songs.js

---
+++ models/songs.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+const { Utils } = require('@tetracyl/erela.js');
+
+module.exports = (duration) => {
+    if(isNaN(duration) || typeof duration === 'undefined') return '00:00';
+    if(duration > 3600000000) return 'Live';
+    return Utils.formatTime(duration, true);
+};
