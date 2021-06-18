33bb5dde36b137b4cce97bc65c2ae7918b1883db
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-18 11:26:31 +0530

    Create Skip.js

---
+++ src/commands/Skip.js
@@ -0,0 +1,27 @@
+const Command = require('../abstract/Command.js');
+
+class Skip extends Command {
+    get name() {
+        return 'skip';
+    }
+
+    get usage() {
+        return 'skip';
+    }
+
+    get description() {
+        return 'Skips the currently playing song';
+    }
+
+    async run(msg) {
+        if (!msg.member.voice.channelID)
+            return await msg.channel.send('Admiral, you are not in a voice channel to perform this');
+        const dispatcher = this.client.queue.get(msg.guild.id);
+        if (!dispatcher)
+            return await msg.channel.send('Nothing is playing in this guild.');
+        if (dispatcher.player.voiceConnection.voiceChannelID !== msg.member.voice.channelID)
+            return await msg.channel.send('Teitoku, you are not in the same voice channel where I am.');
+        await dispatcher.player.stopTrack();
+    }
+}
+module.exports = Skip;
