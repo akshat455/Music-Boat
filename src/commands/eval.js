abdd319e22bb393019feccb7a65c8ace96df4af8
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-18 10:01:32 +0530

    Create eval.js

---
+++ src/commands/eval.js
@@ -0,0 +1,50 @@
+const util = require('util');
+const { MessageEmbed } = require('discord.js');
+const Command = require('../abstract/Command.js');
+
+class Eval extends Command {
+    get name() {
+        return 'eval';
+    }
+
+    get usage() {
+        return 'eval [code]';
+    }
+
+    get description() {
+        return 'Evaluates the given JS statement';
+    }
+
+    get aliases () {
+        return ['eval', 'ev'];
+    }
+
+    get permissions() {
+        return 'OWNER';
+    }
+
+    async run(msg, args) {
+        const code = args.join(' ');
+        if (!code)
+            return await msg.channel.send('Admiral, what is the code you want me to run?');
+        let res;
+        try {
+            res = eval(code);
+            res = util.inspect(res, { depth: 0 });
+        } catch (error) {
+            res = util.inspect(error, { depth: 0 });
+        }
+        const embed = new MessageEmbed()
+            .setColor(this.client.color)
+            .setTitle('Eval Results')
+            .setDescription(`\`\`\`js\n${this.trim(res, 2000)}\`\`\``)
+            .setTimestamp()
+            .setFooter(this.client.user.username, this.client.user.displayAvatarURL());
+        await msg.channel.send(embed);
+    }
+
+    trim(string, max) {
+        return string.length > max ? string.slice(0, max) : string;
+    }
+}
+module.exports = Eval;
