51c7ead184e4f0ca819001c1319016fc5707844f
M s v <78690237+Just-Msv@users.noreply.github.com>
2021-06-18 10:02:27 +0530

    Create Help.js

---
+++ src/commands/Help.js
@@ -0,0 +1,33 @@
+/* eslint-disable linebreak-style */
+
+const Command = require('../abstract/Command.js');
+const util = require('../../util');
+const misc = require('../../misc.json');
+
+class Help extends Command {
+    get name() {
+        return 'help';
+    }
+
+    get usage() {
+        return 'help';
+    }
+
+    get description() {
+        return 'help';
+    }
+
+
+    async run(msg) {
+
+        const embed = util.embed()
+            .setTitle('>  <a:NotaMusical:848214815146115102>  Commands List')
+            .setDescription(`My Prefix is ${misc.prefix}`)
+            .addField('**❯ Music**:','`Play`,`Forward`, `Rewind`, `Lyrics`, `Pause`, `Queue`, `Replay`, `Search`, `Seek`, `Seek2`, `Skip`, `Skipto`, `Stop`, `Ping`, `Remove`, `Removedupes`, `Clear`,  `Volume`, `Shuffle`, `Nowplaying`, `Move`')
+            .addField('**❯ Filters**:','`Bassboost`, `Rate`, `Pitch`, `Speed`, `Rotation`, `Nightcore`, `Vaporwave`,`ClearFilters`')
+            .addField('**❯ Utility**','`help`,`invite`,`stats`')
+        msg.channel.send(embed);
+                
+    }
+}
+module.exports = Help;
